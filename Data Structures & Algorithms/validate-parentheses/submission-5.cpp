0179d7971920716b4a730d91fa6ac68358229bfb
Ayush Agrawal <107486567+ayush-agrawal-19@users.noreply.github.com>
2026-09-20 03:23:54 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> brackets;
+
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                brackets.push(c);
+            }
+            else if (c == ')') {
+                if (!brackets.empty() && brackets.top() == '(') {
+                    brackets.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+            else if (c == ']') {
+                if (!brackets.empty() && brackets.top() == '[') {
+                    brackets.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+            else if (c == '}') {
+                if (!brackets.empty() && brackets.top() == '{') {
+                    brackets.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        if (brackets.empty()) {
+            return true;
+        }
+
+        return false;
+    }
+};
